edf9d01a7f3c89ed4df5a118e0dc5a090a6ab3e2
Keita Yamamoto <keita.yamamoto.1009@gmail.com>
2023-06-24 22:16:15 +0900

    20230624_1

--- backup.command.sh
+++ backup.command.sh
@@ -2,8 +2,8 @@
 
 docker exec forge rcon-cli save-off
 
-tar cvzf mcworldbackup_` date +%Y%m%d`.tar.gz /home/keita/data/minecraft/forge/world/
+tar cvzf /home/keita/data/backup/mcworldbackup_` date +%Y%m%d`.tar.gz /home/keita/data/minecraft/forge/world/
 
-rm  mcworldbackup_` date -d '8 days ago' +%Y%m%d`.tar.gz
+rm  /home/keita/data/backup/mcworldbackup_` date -d '8 days ago' +%Y%m%d`.tar.gz
 
 docker exec forge rcon-cli save-on
--- backup.command.weekly.sh
+++ backup.command.weekly.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-aws s3 cp --storage-class STANDARD_IA mcworldbackup_` date +%Y%m%d`.tar.gz s3://deile-mc-backup
+aws s3 cp --storage-class STANDARD_IA /home/keita/data/backup/mcworldbackup_` date +%Y%m%d`.tar.gz s3://deile-mc-backup
